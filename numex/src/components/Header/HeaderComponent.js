922e838c6571f77976bbfc941e91b40493c96c59
Leonardo Lopes <leoreslopes@gmail.com>
2024-01-12 00:36:01 -0300

    Feat: Header component creation

---
+++ numex/src/components/Header/HeaderComponent.js
@@ -0,0 +1,12 @@
+import "./HeaderComponent.css";
+import LogoComponent from "../Logo/LogoComponent";
+
+function Header() {
+  return (
+    <div className="App-header">
+      <LogoComponent></LogoComponent>
+    </div>
+  );
+}
+
+export default Header;
